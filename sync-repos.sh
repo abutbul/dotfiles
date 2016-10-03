29c4c9ae7764a6433f891676adf70f1cf3cd66f2
david abutbul <david@abutbul.com>
2016-10-03 13:19:44 +0300

    fixing tmux deployment

--- sync-repos.sh
+++ sync-repos.sh
@@ -4,5 +4,5 @@ cd ~/repositories/
 #bring usual suspects
 #dependency dotfiles git
 git clone git@github.com:abutbul/dotfiles.git && echo "should call dot files deployment script here"
-git clone https://github.com/boxcutter/windows.git windows-boxes && mkdir ~/vagrant-temp; ln -s windows-boxes ~/vagrant-temp/
+git clone https://github.com/boxcutter/windows.git windows-boxes 
 git clone https://github.com/tmux-plugins/tpm tpm && chmod +x ~/repositories/dotfiles/dotdep_tmux.sh && ~/repositories/dotfiles/dotdep_tmux.sh
