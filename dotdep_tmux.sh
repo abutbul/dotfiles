3d874b7b291e403441646810e33fbe42bb7dd0e4
david abutbul <david@abutbul.com>
2016-10-03 13:11:31 +0300

    fixing tmux deployment

--- dotdep_tmux.sh
+++ dotdep_tmux.sh
@@ -1,4 +1,11 @@
-mkdir -p ~/.tmux/plugins/
-ln -s ~/repositories/tpm ~/.tmux/plugins/
-ln -s ~/repositories/dotfiles/.tmux.conf ~/
+if [ ! -d ~/.tmux/plugins/tpm ]
+then 
+	mkdir -p ~/.tmux/plugins/
+	ln -s ~/repositories/tpm ~/.tmux/plugins/
+fi
+if [ ! -f ~/.tmux.conf ]
+then
+	ln -s ~/repositories/dotfiles/.tmux.conf ~/
+fi
+mkdir -p ~/.config/systemd/user/ #should be fixed to not require this in future versions of tmux
 tmux source ~/.tmux.conf
