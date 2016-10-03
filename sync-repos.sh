8ebb9c901a21f1cf5fea4289cc401f6287fc49e7
david abutbul <david@abutbul.com>
2016-10-03 12:28:28 +0300

    more deployment for dotfiles

--- sync-repos.sh
+++ sync-repos.sh
@@ -1,2 +1,6 @@
-git clone https://github.com/boxcutter/windows.git windows-boxes
-git clone git@github.com:abutbul/dotfiles.git
+#be in ~/repositories
+cd ~/repositories/
+#bring usual suspects
+git clone https://github.com/boxcutter/windows.git windows-boxes && mkdir ~/vagrant-temp; ln -s windows-boxes ~/vagrant-temp/windows-boxes
+git clone git@github.com:abutbul/dotfiles.git && echo "should call dot files deployment script here"
+git clone https://github.com/tmux-plugins/tpm tpm && ln -s tpm ~/.tmux/plugins/tpm
